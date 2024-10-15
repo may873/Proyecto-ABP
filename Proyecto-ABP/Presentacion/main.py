385408ec070084b6f29b9928ef00fe07f845af58
may873 <vitalliquidanoemmanuel@cetis50cdmx.com>
2024-10-15 00:12:50 -0600

    se sube la versión 1.2 del Proyecto

--- Proyecto-ABP/Presentacion/main.py
+++ Proyecto-ABP/Presentacion/main.py
@@ -2,7 +2,17 @@ import flet as ft
 
 
 def main(page: ft.Page):
-    page.add(ft.SafeArea(ft.Text("Hello, Flet!")))
+    page.title=("Presentacion")
+    page.bgcolor="black"
 
+    lbl1=ft.Text("¿Que es la tecnologia ?")
+    btna1=ft.ElevatedButton("Respuesta Audio")
+    btnt1=ft.ElevatedButton("Respuesta Texto ")
 
+    page.add(
+        ft.Column(
+            controls=[
+            ft.Row(controls=[lbl1],alignment="CENTER"),
+            ft.Row(controls=[btna1,btnt1],alignment="CENTER"),
+            ],alignment="CENTER"))
 ft.app(main)
